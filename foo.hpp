a78a69f1f83c61e5acf54dcf3105ec849d3a7158
Olaf Cichosz <01171645@pw.edu.pl>
2024-12-01 21:52:05 +0100

    zadanie

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,23 @@
-#pragma once
+#ifndef FOO_HPP
+#define FOO_HPP
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+std::vector< char > foo(std::list< Human >& humans)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result;
+
+    for (auto& human : humans) {
+        human.birthday();
+    }
+
+    for (auto it = humans.rbegin(); it != humans.rend(); ++it) {
+        result.push_back(it->isMonster() ? 'n' : 'y');
+    }
+
+    return result;
 }
+
+#endif // FOO_HPP
